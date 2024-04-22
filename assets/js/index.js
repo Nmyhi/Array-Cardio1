2737c56d424f7c26e33d95d0ca754dff4983f5c7
Mihaly Lovrencsics <lomysa@gmail.com>
2024-04-22 21:05:02 +0000

    Solve the 3rd task

--- assets/js/index.js
+++ assets/js/index.js
@@ -47,6 +47,31 @@ console.table(firstAndLastNames);
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 
+const oldestToYoungest = inventors.sort((a,b) => a.year - b.year);
+console.table(oldestToYoungest);
+// function
+
+const sorted = inventors.sort(function(a, b) {
+    if(a.year > b.year) {
+        return 1;
+    } else {
+        return -1;
+    }
+});
+
+console.table(sorted);
+
+// Sort inventors by age
+
+inventors.forEach((inventor) => {
+    inventor.age = inventor.passed - inventor.year;
+  });
+  
+
+  const sortedByAge = inventors.sort((a, b) => a.age - b.age);
+  
+  console.table(sortedByAge);
+
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
 
